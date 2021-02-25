604e92f9fc91000792cc27fe179a8a8a3773373c
Vatsal <vatsalpatel1073@gmail.com>
2021-02-25 10:24:12 +0530

    maxSubarray sum

--- Problem Solving Patterns/Sldiing Window Pattern/MaxSubarraySum.py
+++ Problem Solving Patterns/Sldiing Window Pattern/MaxSubarraySum.py
@@ -1,2 +1,43 @@
+
+""" 
+Given an array of integers Arr of size N and a number K. Return the maximum sum of a subarray of size K.
+
+Example:
+
+Input:
+N = 4, K = 2
+Arr = [100, 200, 300, 400]
+Output:
+700
+Explanation:
+Arr3  + Arr4 =700,
+which is maximum.
+
+"""
+
 def maxSum(arr, k):
-    pass
+    
+    windowSum = maxSum = sum(arr[:k])
+    
+    print(windowSum, 'is window sum for first k elements')
+    
+    for num in range(k, len(arr)):
+       windowSum += arr[num] - arr[num - k]
+       print(windowSum, ' is windowSum for next k elements')
+       maxSum = max(maxSum, windowSum)
+       
+    return maxSum
+    
+    
+    
+    
+
+
+s = maxSum([1, 4, 2, 10, 2, 3, 1, 0, 20], 4)
+print(s)
+
+
+
+
+
+
