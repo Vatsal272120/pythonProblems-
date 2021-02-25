8f35adcb5d40afb4357ab5a4a672ef4f5d3d82b1
Vatsal <vatsalpatel1073@gmail.com>
2021-02-25 18:20:21 +0530

    control statements

---
+++ Control Statements/While.py
@@ -0,0 +1,4 @@
+x = 0
+while x < 5:
+    print('current value of x is ', x)
+    x+=1
---
+++ Control Statements/for.py
@@ -0,0 +1,13 @@
+d = {
+    'k' : 1,
+    'l' : 2,
+    'm' : 3
+}
+
+for item in d:
+    print(item, 'keys')
+    
+
+for key, item in d.items():
+    
+    print(key, ' tuple pair')
